133035d6a574fb4d3d3a1b09ad9b027ad499056b
Philip Schwartz <mail@pschwartz.de>
2019-07-03 19:07:08 +0200

    Umfrage inaktiviert.

--- senden.php
+++ senden.php
@@ -1,9 +1,9 @@
-<?php iconv_set_encoding("internal_encoding", "UTF-8");
-	if(!empty($_POST)) {
-		mail("Kurs-Umfrageergebnisse WinterAka 18/19 <umfrage_kurs_winter1819@pschwartz.de>, ", '=?utf-8?B?'.base64_encode("[winter1819] Umfrage von " . $_POST["Name"]).'?=', print_r($_POST, TRUE), "Content-Type: text/plain; charset=UTF-8");
-		mail("Benjamin Haake <ben_haake@web.de>, ", '=?utf-8?B?'.base64_encode("[winter1819] Umfrage von " . $_POST["Name"]).'?=', print_r($_POST, TRUE), "Content-Type: text/plain; charset=UTF-8");
-	}
-?>
+<!--<?php #iconv_set_encoding("internal_encoding", "UTF-8");
+#	if(!empty($_POST)) {
+#		mail("Kurs-Umfrageergebnisse WinterAka 18/19 <umfrage_kurs_winter1819@pschwartz.de>, ", '=?utf-8?B?'.base64_encode("[winter1819] Umfrage von " . $_POST["Name"]).'?=', print_r($_POST, TRUE), "Content-Type: text/plain; charset=UTF-8");
+#		mail("Benjamin Haake <ben_haake@web.de>, ", '=?utf-8?B?'.base64_encode("[winter1819] Umfrage von " . $_POST["Name"]).'?=', print_r($_POST, TRUE), "Content-Type: text/plain; charset=UTF-8");
+#	}
+?>-->
 
 <!DOCTYPE HTML>
 <html>
@@ -15,6 +15,6 @@
 <body>
 <h1 style="text-align:center">CdE-WinterAkademie 2018/19 – „Einführung in die Differentialgeometrie“: Teilnehmerumfrage</h1>
 
-<div>Deine Angaben wurden gesendet. Vielen Dank!</div>
+<div>Die Umfrage ist nicht mehr aktiv.</div>
 </body>
 </html>
